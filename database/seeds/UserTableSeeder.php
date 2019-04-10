6a37a947d071c37e552c5d3cd0df11a5a6e55d28
MXC1 <mfhcor@gmail.com>
2019-04-10 10:03:28 +0100

    Created Faker for populating user table

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\User;
+use Carbon\Carbon;
 
 class UserTableSeeder extends Seeder
 {
@@ -11,6 +14,22 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+		
+		$users = User::all()->pluck('id')->toArray();
+		
+		foreach (range(1,10) as $index) {
+			DB::table('users')->insert([
+				'id' => $faker->numberBetween($min = 1, $max = 9999),
+				'name' => $faker->firstName,
+				'email' => $faker->email,
+				'role' => $faker->numberBetween(0,1),
+				'password' => Hash::make($faker->word),
+				'created_at' => $current_date_time = Carbon::now()->toDateTimeString(),
+				'updated_at' => $current_date_time = Carbon::now()->toDateTimeString()
+				]);
+		}
+	}
+				
     }
-}
+?>
