e8d3b15dd14464457e943f4edf5ec571555d193d
MXC1 <mfhcor@gmail.com>
2019-04-20 16:01:57 +0100

    Changed title from Laravel to Aston Animal Sanctuary

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Aston Animal Sanctuary') }}</title>
+    <title>Aston Animal Sanctuary</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Aston Animal Sanctuary') }}
+                    Aston Animal Sanctuary
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -43,10 +43,12 @@
 						<a class="nav-link" href="{{ url('animals/create') }}">Add Animal </a>
 						<li class="nav-item">
 						<a class="nav-link" href="{{ url('users') }}">Users </a>
+						<li class="nav-item">
+						<a class="nav-link" href="{{ url('adoptions') }}">Adoptions </a>
 						@endif
 						</li>
 						<li class="nav-item">
-						<a class="nav-link" href="{{ url('requests') }}">Requests </a>
+						<a class="nav-link" href="{{ url('requests') }}">Pending Requests </a>
 						</li>
 						@endguest
                     </ul>
