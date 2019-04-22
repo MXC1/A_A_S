488fe5efb773749d4f117660e12994cd3b27e0eb
MXC1 <mfhcor@gmail.com>
2019-04-22 18:53:27 +0100

    Secure_Asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-	<link rel="stylesheet" href="{{ secure_asset('css/AdminLTE.min.css') }}">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
